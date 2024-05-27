26678d1b8a2da9be1b01df2530a7268e80ebe750
Brayan Moreno <112002842+UTS23@users.noreply.github.com>
2024-05-27 18:26:13 -0500

    Delete ULTIMO_PROYECTO directory

--- ULTIMO_PROYECTO/src/main/java/com/papeleria/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.papeleria.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.papeleria.entity.Admin;
-
-public interface AdminRepository extends MongoRepository<Admin, String> {
-    Admin findByEmail(String email);
-}
--- ULTIMO_PROYECTO/src/main/java/com/papeleria/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.papeleria.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.papeleria.entity.Cliente;
-
-public interface ClienteRepository extends MongoRepository<Cliente, String> {
-    Cliente findByEmail(String email);
-}
